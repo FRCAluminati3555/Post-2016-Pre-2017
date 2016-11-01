b855fa3ea7f22f16491f0ac75797f9abffe3e361
FRC3555 <frcaluminati@gmail.com>
2016-11-01 18:53:46 -0400

    Compitition code! The java version was used during Brawl

---
+++ Post 2016 Java/src/org/usfirst/frc/team3555/robot/MotorGroup.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team3555.robot;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class MotorGroup {
+	/*
+	 * these are the fields for the passed in speed controllers
+	 */
+	private SpeedController speedController1;
+	private SpeedController speedController2;
+	
+	/*
+	 * constructor of the MotorGroup class
+	 * this takes in 2 speed controllers, only two because that's all that is used on the robot
+	 */
+	public MotorGroup(SpeedController speedController1, SpeedController speedController2){
+		this.speedController1 = speedController1;
+		this.speedController2 = speedController2;
+	}
+	
+	/*
+	 * This method sets the speed of the speed controllers using their set method, given by the speed controller class
+	 */
+	public void set(double speed){
+		speedController1.set(speed);
+		speedController2.set(speed);
+	}
+	
+	/*
+	 * This method returns the speed of the speed controllers using their get method, given by the speed controller class
+	 * only one speed controller is used here because both of the controllers are at the same speed, 
+	 * and only one value can be returned
+	 */
+	public double get(){
+		return speedController1.get();
+	}
+}
--- Post 2016 Java/src/org/usfirst/frc/team3555/robot/Robot.java
+++ Post 2016 Java/src/org/usfirst/frc/team3555/robot/Robot.java
@@ -2,15 +2,18 @@ package org.usfirst.frc.team3555.robot;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /*
  * creates the robot class and extends the SampleRobot class
  * the Sample robot class gives the operator control, test method, etc..
  */
-
 public class Robot extends SampleRobot {
 	
 	/*
@@ -22,20 +25,63 @@ public class Robot extends SampleRobot {
 	/*
 	 * Each side has 2 motors that turns 1 shaft.
 	 * Used to give that shaft a greater power.
+	 * 
+	 * Motor Group is a team made class that controlles motors in a group format
+	 * Motor Group allows the two motors to be controlled at the same speed, and not allow them to be set to different speeds
 	 */
 	
-	private Spark driveSparkL1; //left side 
-	private SlaveSpark driveSparkL2; //left side slave
-	
-	private Spark driveSparkR1; //right side
-	private SlaveSpark driveSparkR2; //right side slave 
+	private MotorGroup leftSideDrive;
+	private MotorGroup rightSideDrive;
 	
 	
 	/*
-	 * Creates 2 new joy stick objects
+	 * the potentiometer measures angle
+	 * This potentiometer measures the angle of the vertival alignment of the shooter
+	 * 
+	 * Currently not being used because of time constraints
+	 */
+//	private AnalogPotentiometer verticalPoten;
+	
+	/*
+	 * These constraints keep the robot from hiting either end
+	 */
+//	private double verticalDownMax;
+//	private double verticalUpMax;
+	
+	
+	/*
+	 * These speed controllers control the shooter motors
+	 */
+	
+	Victor shoot1;  
+	Talon shoot2; 
+	
+	/*
+	 * Creates 3 new joy stick objects
+	 * 
+	 * The side joysticks control each side of the robot (Depending on the drive method)
 	 */
 	private Joystick joyLeft;
 	private Joystick joyRight;
+	private Joystick joyOP;
+	
+	/*
+	 * The relay is a motor that can go either foward or backward
+	 * Since the loader only needs to go these two directions, the relay is used
+	 */
+	private Relay loader;
+
+	
+	/*
+	 * Speed controller that controls the motor on the vertical alignment
+	 */
+	private Spark vertAd;
+	
+	
+	/*
+	 * This variable tells wether or not arcade dirive is being used
+	 */
+	private boolean arcadeDrive;
 	
 	/*
 	 * When someone lets go of the joy stick, the joy stick doesn't end up at perfectly 0
@@ -43,7 +89,7 @@ public class Robot extends SampleRobot {
 	 * Over time, sending it this instruction breaks the motor, and draws power.
 	 * The dead zone is the area of the joy stick that the joy stick won't send the speed controller instructions.
 	 */
-	private final static double DEADZONE = .08;
+	private final static double DEADZONE = .08, LOADZONE = .8;
 	
 	/*
 	 * Class constructor, this is called when the program starts.
@@ -54,14 +100,20 @@ public class Robot extends SampleRobot {
     	 * The Sparks take in a port number from the PWM pins on the RoboRIO
     	 * The joysticks each take in port numbers, this number can be found in the driver station when joystick is plugged in
     	 */
-    	driveSparkL1 = new Spark(0);
-    	driveSparkL2 = new SlaveSpark(1, driveSparkL1);
+    	leftSideDrive = new MotorGroup(new Spark(0), new Spark(1));
+    	rightSideDrive = new MotorGroup(new Spark(2), new Spark(3));
     	
-    	driveSparkR1 = new Spark(2);
-    	driveSparkR2 = new SlaveSpark(3, driveSparkR1);
+    	shoot1 = new Victor(5);
+    	shoot2 = new Talon(6);
     	
-    	joyLeft = new Joystick(0);
-    	joyRight = new Joystick(3);
+    	loader = new Relay(2);
+		vertAd = new Spark(4);
+    	
+    	joyOP = new Joystick(0);
+    	joyLeft = new Joystick(1);
+    	joyRight = new Joystick(2);
+    	
+//    	verticalPoten = new AnalogPotentiometer(0);
     	
     	camera.setQuality(50);
     	camera.startAutomaticCapture("cam0");
@@ -72,7 +124,7 @@ public class Robot extends SampleRobot {
      * then it will tell the motors to get at the speed the joystick say.
      * Tank drive is controlled with 2 different joysticks and each joystick controls a side 
      */ 
-    public void tankDrive(){
+	public void tankDrive(){
     	double leftSpeed = 0;
     	double rightSpeed = 0;
     	
@@ -84,15 +136,28 @@ public class Robot extends SampleRobot {
     		rightSpeed = -joyRight.getRawAxis(1);
     	}
     	
-    	driveSparkL1.set(leftSpeed);
-    	driveSparkL2.update();
+    	leftSideDrive.set(leftSpeed);
+    	rightSideDrive.set(rightSpeed);
     	
-    	driveSparkR1.set(rightSpeed);
-		driveSparkR2.update();
-		
 		Timer.delay(0.005);
     }
     
+    /*
+     * This method is called by the field at the comptition
+     * This will just drive the robot foward at 75% Speed for the set amount of seconds, the local variable seconds
+     */
+    public void autonomous() {
+    	int seconds = 3;
+    	long startTime = System.currentTimeMillis();
+    	
+    	while(System.currentTimeMillis() < startTime + seconds * 1000){
+    		leftSideDrive.set(-.75);
+    		rightSideDrive.set(.75);
+    	}
+    	
+    	leftSideDrive.set(0);
+    	rightSideDrive.set(0);
+	}
     
     /*
      * arcade drive is controlled by only one joysitck
@@ -107,27 +172,107 @@ public class Robot extends SampleRobot {
     		rightSpeed = -arcadeJoy.getRawAxis(1) - arcadeJoy.getRawAxis(0);
     	}
     	
-    	driveSparkL1.set(leftSpeed);
-		driveSparkL2.update();
-		
-		driveSparkR1.set(rightSpeed);
-		driveSparkR2.update();
-		
-//		SmartDashboard.putDouble("Left", driveSparkL1.get());
-//		SmartDashboard.putDouble("Left slave", driveSparkL2.get());
-//		
-//		SmartDashboard.putDouble("Right", driveSparkR1.get());
-//		SmartDashboard.putDouble("Right slave", driveSparkR2.get());
+    	leftSideDrive.set(leftSpeed);
+    	rightSideDrive.set(rightSpeed);
+    	
+		Timer.delay(0.005);
     }
     
+	/*
+	 * This method checks if the buttons are being pressed
+	 * if so, adjust the drive method accordingly
+	 */
+	public void checkButtons(){
+		if(joyRight.getRawButton(6) || joyLeft.getRawButton(6)){
+    		arcadeDrive = false;
+    	}
+    	
+		else if(joyRight.getRawButton(7) || joyLeft.getRawButton(6)){
+    		arcadeDrive = true;
+    	}
+	}
+	
+	/*
+	 * This will update the speed of the drive motors
+	 */
+	public void updateDrive(){
+		checkButtons();
+		
+		if(arcadeDrive){
+    		SmartDashboard.putString("You are currently using: ", "Arcade Drive");
+    		arcadeDrive(joyRight);
+    	}
+    		
+    	else{
+    		SmartDashboard.putString("You are currently using: ", "Tank Drive");
+    		tankDrive();
+    	}
+	}
+	
+	/*
+	 * This method updates the state of the shooter
+	 */
+	public void updateShooter(){
+		if(joyOP.getRawAxis(3) <= -LOADZONE){
+    		loader.set(Relay.Value.kReverse);
+    	}
+    	
+    	else if(joyOP.getRawAxis(3) >= LOADZONE){
+    		loader.set(Relay.Value.kForward);
+    	}
+    	
+    	else{
+    		loader.set(Relay.Value.kOff);
+    	}
+    	
+    	if(joyOP.getRawButton(1) && Math.abs(joyOP.getRawAxis(1)) >= DEADZONE){
+    		shoot1.set(joyOP.getRawAxis(1));
+    		shoot2.set(-joyOP.getRawAxis(1));
+    		loader.set(Relay.Value.kForward);
+    	}
+    	
+    	else{
+    		shoot1.set(0);
+    		shoot2.set(0);
+    	}
+    	
+    	if(joyOP.getRawButton(4) && Math.abs(joyOP.getRawAxis(2)) >= DEADZONE){
+    		vertAd.set(joyOP.getRawAxis(2));
+    	}
+    	
+    	else
+    		vertAd.set(0);
+    	
+//    	if(verticalPoten.get() <= verticalDownMax){
+//		
+//		}
+//		
+//		if(verticalPoten.get() >= verticalUpMax){
+//			
+//		}
+	}
+	
+	/*
+	 * Prints any data that is needed to the Smart Dashboard
+	 */
+	@SuppressWarnings("deprecation")
+	public void printData(){
+//    	SmartDashboard.putDouble("Shooter 0,1: ", shooterMotors.get());
+//    	SmartDashboard.putDouble("Vertical Potentiometer: ", verticalPoten.get());
+//    	SmartDashboard.putDouble("Vertical 3: ", vertAd.get());
+		SmartDashboard.putDouble("Left Side: ", leftSideDrive.get());
+    	SmartDashboard.putDouble("Right Side: ", rightSideDrive.get());
+	}
+	
     /*
      * This while loop runs when the operator control is on and the robot is enabled
      * the robot is updated in the loop by taking in inputs and such...
      */
-    public void operatorControl() {
+	public void operatorControl() {
         while (isOperatorControl() && isEnabled()) {
-        	arcadeDrive(joyRight);
-//        	tankDrive();
+        	updateDrive();
+        	updateShooter();
+        	printData();
         }
     }
 }
